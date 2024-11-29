18a70d940018063ad7c5ca8891cec92254fe3510
MD NASIM <156278474+nasimubd@users.noreply.github.com>
2024-11-29 17:40:46 +0600

    All the needed Models are created

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// routes/web.php or routes/api.php
+Route::get('/register', [AuthController::class, 'register']);
+Route::get('/members', [MemberController::class, 'list']);
+Route::get('/meal-plans', [MealPlanController::class, 'list']);
+Route::get('/deliveries', [DeliveryController::class, 'list']);
+
